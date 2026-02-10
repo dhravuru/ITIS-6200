69932db80f7539a9872ee12ac72775a0635f16aa
Dhanush Ravuru <dhravuru@gmail.com>
2026-02-09 21:24:55 -0500

    Added traverse_directory() function

--- Lab02HashingProgram/Lab02HashingProgram.py
+++ Lab02HashingProgram/Lab02HashingProgram.py
@@ -1,17 +1,27 @@
 import os
 import hashlib
 import json
+from pathlib import Path
 
-def hash_file(file_path):
-    sha256_hash_alg = hashlib.sha256()
+def hash_file(file_path, algorithm="sha256"):
+    hash_function = hashlib.new(algorithm)
     try:
         with open(file_path, "rb") as file:
             while chunk := file.read(8192):
-                sha256_hash_alg.update(chunk)
-        return sha256_hash_alg.hexdigest()
+                hash_function.update(chunk)
+        return hash_function.hexdigest()
 
     except Exception as e:
         print(f"Error hashing file {file_path}: {e}")
         return None
     
-    
+def traverse_directory(dir_path):
+    hash_table = {}
+    for root, _, files in os.walk(dir_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_hash = hash_file(file_path)
+            if file_hash:
+                hash_table[file_path] = file_hash
+    return hash_table
+
